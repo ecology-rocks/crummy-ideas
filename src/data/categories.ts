6b3fb4ff298029c05799088f13343ae656a40f31
Sam L. Davis, Ph.D <ecology-rocks@users.noreply.github.com>
2023-12-03 19:42:24 -0500

    Initial commit

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Community Cat Care',
+	'Indoor Cat Care',
+	'Veterinary Care',
+	'Cat Guardian Resources'
+] as const
---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+	paginationSize: number
+}
+
+export const siteConfig: SiteConfig = {
+	author: 'Sam Davis', // Site author
+	title: 'Street Cat Rescue Network', // Site title.
+	description: 'Helping cat guardians care for indoor and outdoor cats.', // Description to display in the meta tags
+	lang: 'en-US',
+	ogLocale: 'en_US',
+	shareMessage: 'Share this post', // Message to share a post on social media
+	paginationSize: 6 // Number of posts per page
+}
